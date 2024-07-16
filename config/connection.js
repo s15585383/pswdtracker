b8dcabfa5b7bb3fa6f1794048598b9b1b8d64d36
Samuel Leite <samuelleite.fl@gmail.com>
2024-07-15 20:20:21 -0400

    render

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,7 @@ const express = require('express');
 const { Sequelize } = require('sequelize');
 
 const app = express();
-// const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
 let sequelize;
 
