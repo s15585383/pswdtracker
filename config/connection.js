06e047e2cd94b821e7b63e03d1de3d7be77ccd08
Samuel Leite <samuelleite.fl@gmail.com>
2024-07-15 22:03:26 -0400

    render

--- config/connection.js
+++ config/connection.js
@@ -11,25 +11,20 @@ if (process.env.DB_URL) {
   sequelize = new Sequelize(process.env.DB_URL, {
     dialect: 'postgres',
     logging: false,
-    // dialectOptions: {
-    // ssl: {
-    //   require: true,
-    //   rejectUnauthorized: false,
-    //   },
-    // },
   });
-} else {
-  sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-      host: 'localhost',
-      dialect: 'postgres',
-      port: '5432',
-    }
-  );
 }
+// } else {
+//   sequelize = new Sequelize(
+//     process.env.DB_NAME,
+//     process.env.DB_USER,
+//     process.env.DB_PASSWORD,
+//     {
+//       host: 'localhost',
+//       dialect: 'postgres',
+//       port: '5432',
+//     }
+//   );
+// }
 
 // sequelize
 //   .sync()
